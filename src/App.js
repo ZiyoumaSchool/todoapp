0697b60c409ad3c871431c0a5cdd0baaf8873817
tognia <h.tognia@ziyouma-agency.com>
2021-11-23 16:37:05 +0100

    Modification Apparence Login page and Home 23 nov 2021 0.2.0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 import store from './lib/redux';
 
-//import InboxScreen from './components/InboxScreen';
+import InboxScreen from './components/InboxScreen';
 import Banner from './components/Banner';
 import Login from './components/Login';
 import Home from './components/Home';
@@ -25,11 +25,16 @@ function App(){
   return (
   <div className="app">
     <Banner/>
-    
-    {user ? <Home user={user} /> : <Login/>}
+    {user ? 
+    <div>
+    <Home user={user} /> 
     <Provider store={store}>
-      {/*<InboxScreen />*/}
+    <InboxScreen />      
     </Provider>
+    </div>
+    : <Login/>
+    }
+    
   </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import '../App.css';
 
 const Login = () => {
   return (
-    <div>
+    <div align="center">
       <button className="button" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
     </div>
   )
